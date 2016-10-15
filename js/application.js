5dbe40b601c04bc070d770c7c9f6a1398e9e05b0
Jose Lechuga <joslec9@gmail.com>
2016-10-14 20:02:07 -0400

    jQuery assignment two
    jQuery Assigment two coding dojo

---
+++ js/application.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+			$('#ninja').click(function(){
+				$(this).hide();
+			})
+			$('#ninja2').click(function(){
+				$(this).hide();
+			})
+			$('#ninja3').click(function(){
+				$(this).hide();
+			})
+			$('#ninja4').click(function(){
+				$(this).hide();
+			})
+			$('#ninja5').click(function(){
+				$(this).hide();
+			})
+			$('#ninja6').click(function(){
+				$(this).hide();
+			})
+			$('#ninja7').click(function(){
+				$(this).hide();
+			})
+			$('#ninja8').click(function(){
+				$(this).hide();
+			})
+
+			$('button').click(function(){
+				$('img').show();
+			})
+
+
+
+
+		})
