9466df6a86991f1065866a5ae9219dd0954acad7
Jose Lechuga <joslec9@gmail.com>
2016-10-15 22:56:16 -0400

    update
    update

--- js/application.js
+++ js/application.js
@@ -1,34 +1,38 @@
-$(document).ready(function(){
-			$('#ninja').click(function(){
+$(document).ready(function() {
+	
+			$('#ninja1').click(function() {
 				$(this).hide();
 			})
-			$('#ninja2').click(function(){
+			
+			$('#ninja2').click(function() {
 				$(this).hide();
 			})
-			$('#ninja3').click(function(){
+			
+			$('#ninja3').click(function() {
 				$(this).hide();
 			})
-			$('#ninja4').click(function(){
+			
+			$('#ninja4').click(function() {
 				$(this).hide();
 			})
-			$('#ninja5').click(function(){
+			
+			$('#ninja5').click(function() {
 				$(this).hide();
 			})
-			$('#ninja6').click(function(){
+			
+			$('#ninja6').click(function() {
 				$(this).hide();
 			})
-			$('#ninja7').click(function(){
+			
+			$('#ninja7').click(function() {
 				$(this).hide();
 			})
-			$('#ninja8').click(function(){
+			
+			$('#ninja8').click(function() {
 				$(this).hide();
 			})
 
-			$('button').click(function(){
+			$('button').click(function() {
 				$('img').show();
 			})
-
-
-
-
 		})
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Coding Dojo Project Assignment two
+## Disappearing Ninja
+### Jose Lechuga
+
+
+The image used in this project is provided by (http://w10.naukri.com/mailers/2014/firstnaukri/ninja8sep/images/top1.jpg)
